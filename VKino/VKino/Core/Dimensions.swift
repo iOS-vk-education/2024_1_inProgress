10d9e6fc3b9c80ce562cc05ba763a6064f783025
MrFlop0 <kaplenkovsasha03@gmail.com>
2024-11-28 23:48:11 +0300

    Added dimensions

---
+++ VKino/VKino/Core/Dimensions.swift
@@ -0,0 +1,25 @@
+//
+//  Dimensions.swift
+//  VKino
+//
+//  Created by Aleksandr Kaplenkov on 28.11.2024.
+//
+
+import UIKit
+
+struct Dimensions {
+    
+    struct Spacing {
+        static let xxSmall: CGFloat = 4.0
+        static let xSmall: CGFloat = 8.0
+        static let small: CGFloat = 12.0
+        static let normal: CGFloat = 16.0
+        static let large: CGFloat = 20.0
+    }
+    
+    struct CornerRadius {
+        static let small: CGFloat = 4.0
+        static let normal: CGFloat = 6.0
+        static let large: CGFloat = 8.0
+    }
+}
