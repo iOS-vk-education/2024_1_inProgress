f68448ecda45f435efdbc589f70996963d5453ad
progeranna <95630533+ann-pronkina@users.noreply.github.com>
2024-12-24 01:57:59 +0300

    VK-15_fixed_themes_and_localization_deleted_comments

--- VKino/VKino/Presentation/SettingsScreen/SettingsView.swift
+++ VKino/VKino/Presentation/SettingsScreen/SettingsView.swift
@@ -14,6 +14,5 @@ struct SettingsView: UIViewControllerRepresentable {
     }
 
     func updateUIViewController(_ uiViewController: SettingsViewController, context: Context) {
-        // Обновите контроллер, если необходимо
     }
 }
